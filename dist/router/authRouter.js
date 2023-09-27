4aa7ad0671e5ead9ed06839901e78b33bc322db8
ibrahiminlx <33986500+ibrahiminlx@users.noreply.github.com>
2023-09-27 22:19:37 +0300

    user and post db transaction create

--- dist/router/authRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("../controller/authController");
-const router = (0, express_1.Router)();
-router.get('/login', authController_1.loginController);
-router.get('/tokenGenerate', authController_1.tokenGenerateController);
-router.get('/logout', authController_1.logoutController);
-exports.default = router;
